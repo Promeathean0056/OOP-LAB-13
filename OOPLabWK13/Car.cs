8381ad9e305014409fb50b7d3f69bf551be8b1bf
Promeathean0056 <34377463+Promeathean0056@users.noreply.github.com>
2018-12-06 17:45:44 -0500

    Add files via upload

---
+++ OOPLabWK13/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLabWK13
+{
+    class Car
+    {
+        private int Speed =  0;
+        private string Make;
+        private string Model;
+        private int Year;
+
+        public Car(string pMake, string pModel, int pYear, int pSpeed)
+        {
+            this.Make = pMake;
+            this.Model = pModel;
+            this.Year = pYear;
+            this.Speed = pSpeed;
+        }
+        public Car(string pMake, string pModel, int pYear)
+        {
+            this.Make = pMake;
+            this.Model = pModel;
+            this.Year = pYear;
+            this.Speed = 0;
+        }
+        public int SpeedUp()
+        {
+            this.Speed++;
+            return Speed;
+        }
+        public int SlowDown()
+        {
+            this.Speed--;
+            return Speed;
+        }
+        public void Display()
+        {
+            Console.WriteLine(Year + " " + Make + " " + Model + " is going " + Speed + " MPH.");
+        }
+    }
+}
